5ab85414f1959988cab340685d2c02b4de3809ac
Alex McHugh <alexmchughnz@icloud.com>
2023-12-04 22:30:29 +1300

    Add template and update main pkg message.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+const MESSAGE: &str = r"======== ADVENT OF CODE 2023 ========
+Run `cargo solve day<x>` to run a solution.
+";
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", MESSAGE);
 }
---
+++ src/template.rs
@@ -0,0 +1,16 @@
+use aoc2023::{get_day_str, read_input};
+
+fn part_one(_input: &str) -> Option<u32> {
+    None
+}
+
+fn part_two(_input: &str) -> Option<u32> {
+    None
+}
+
+fn main() {
+    let input = read_input(file!(), "input");
+    let day = get_day_str(file!());
+    println!("{day}-1 solution: {:?}", part_one(&input));
+    println!("{day}-2 solution: {:?}", part_two(&input));
+}
