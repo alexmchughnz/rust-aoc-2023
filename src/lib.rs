1536594903f135edd07debea07c3af1ad44cea2b
Alex McHugh <alexmchughnz@icloud.com>
2023-12-05 23:38:15 +1300

    `read_input` prints target path.

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,8 @@ use std::fs;
 pub fn read_input(path: &str, filename: &str) -> String {
     let mut input_path = path.trim_end_matches(|c| c != '/').to_string();
     input_path += filename;
-    let input = fs::read_to_string(input_path).expect("should read specified input file");
+    let input = fs::read_to_string(&input_path)
+        .expect(format!("should read specified input file ({})", &input_path).as_str());
     input
 }
 
