f79ceb1ecccf1a20cf63f6cd034b0e89f5c3b2cb
Alex McHugh <alexmchughnz@icloud.com>
2023-12-10 16:15:55 +1300

    Day 4-2: Map prize IDs for each card.

--- src/bin/day4/main.rs
+++ src/bin/day4/main.rs
@@ -1,5 +1,10 @@
 use aoc2023::{get_day_str, read_input};
-use std::{collections::HashSet, str::FromStr, time::Instant};
+use std::{
+    collections::{HashMap, HashSet},
+    ops::RangeInclusive,
+    str::FromStr,
+    time::Instant,
+};
 
 #[derive(Debug)]
 struct Card {
@@ -61,12 +66,42 @@ fn part_one(input: &str) -> Option<u32> {
     Some(scores.into_iter().sum())
 }
 
-fn part_two(_input: &str) -> Option<u32> {
-    None
+fn part_two(input: &str) -> Option<u32> {
+    let mut card_counts = HashMap::<usize, u32>::new();
+    let mut card_prize_ids = HashMap::<usize, Option<RangeInclusive<usize>>>::new();
+
+    let all_cards: Vec<Card> = input.lines().map(|line| line.parse().unwrap()).collect();
+    for ref card in all_cards {
+        // Count card.
+        assert!(card_counts.get(&card.id).is_none());
+        card_counts.insert(card.id, 1);
+
+        // Determine the card ids won by this card.
+        let card_nums: HashSet<_> = card.card_nums.iter().collect();
+        let winning_nums: HashSet<_> = card.winning_nums.iter().collect();
+
+        let num_winners = card_nums.intersection(&winning_nums).count();
+        let prize_ids = {
+            if num_winners == 0 {
+                None
+            } else {
+                Some((card.id + 1)..=(card.id + num_winners))
+            }
+        };
+
+        // Add prize ids to map.
+        assert!(card_prize_ids.get(&card.id).is_none());
+        card_prize_ids.insert(card.id, prize_ids);
+    }
+
+    dbg!(&card_counts);
+    dbg!(&card_prize_ids);
+
+    Some(card_counts.values().into_iter().sum())
 }
 
 fn main() {
-    let input = read_input(file!(), "input.txt");
+    let input = read_input(file!(), "example1.txt");
     let day = get_day_str(file!());
 
     let time = Instant::now();
