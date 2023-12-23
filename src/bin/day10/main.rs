9c4e2eb816eb8a276a854739ca070293012dfe04
Alex McHugh <alexmchughnz@icloud.com>
2023-12-23 14:12:59 +1300

    Day 10-1: Complete.

--- src/bin/day10/main.rs
+++ src/bin/day10/main.rs
@@ -1,15 +1,43 @@
-use aoc2023::helpers::grid::GridDirection;
-use aoc2023::helpers::grid::GridDirection::*;
 use aoc2023::{get_day_str, read_input};
-use std::{collections::HashMap, time::Instant};
+use std::time::Instant;
 
-struct Tile {
-    symbol: char,
-    connect: dyn Fn(GridDirection) -> Option<GridDirection>,
-}
+use aoc2023::helpers::grid::Grid;
 
-fn part_one(_input: &str) -> Option<u32> {
-    None
+mod symbols;
+fn part_one(input: &str) -> Option<u32> {
+    let grid: Grid<char> = input.parse().unwrap();
+
+    // Find the loop start and a valid direction to move in.
+    let start_index = grid.find('S').unwrap();
+    let all_movements = grid
+        .adjacent_indices(start_index)
+        .into_iter()
+        .collect::<Vec<_>>();
+
+    let valid_movements = all_movements
+        .into_iter()
+        .filter(|(dir, index)| {
+            if index.is_none() {
+                return false;
+            }
+            let symbol = grid[index.unwrap()];
+            symbols::traverse_pipe(*dir, symbol).is_some()
+        })
+        .collect::<Vec<_>>();
+    let mut pipe_dir = valid_movements[0].0;
+
+    // Follow the pipes until the start index is reached again.
+    let mut current = start_index.clone().step(pipe_dir, &grid).unwrap();
+    let mut num_steps: u32 = 1;
+    while current != start_index {
+        let symbol = grid[current];
+        pipe_dir = symbols::traverse_pipe(pipe_dir, symbol).unwrap();
+        current = current.step(pipe_dir, &grid).unwrap();
+        num_steps += 1;
+    }
+
+    let farthest_distance = num_steps / 2;
+    Some(farthest_distance)
 }
 
 fn part_two(_input: &str) -> Option<u32> {
