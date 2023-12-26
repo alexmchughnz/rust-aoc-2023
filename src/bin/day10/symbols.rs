b70ca7d4a353398fa073f006f4dec944205f16ab
Alex McHugh <alexmchughnz@icloud.com>
2023-12-27 12:27:34 +1300

    Day 10-2: New attempt, added 'map_inward' functions.

--- src/bin/day10/symbols.rs
+++ src/bin/day10/symbols.rs
@@ -3,18 +3,30 @@ use aoc2023::helpers::grid::GridDirection::*;
 
 pub fn traverse_pipe(dir: GridDirection, symbol: char) -> Option<GridDirection> {
     match symbol {
-        '|' => vertical_pipe(dir),
-        '-' => horizontal_pipe(dir),
-        'L' => north_east_bend(dir),
-        'J' => north_west_bend(dir),
-        '7' => south_west_bend(dir),
-        'F' => south_east_bend(dir),
+        '|' => traverse_vertical_pipe(dir),
+        '-' => traverse_horizontal_pipe(dir),
+        'L' => traverse_north_east_bend(dir),
+        'J' => traverse_north_west_bend(dir),
+        '7' => traverse_south_west_bend(dir),
+        'F' => traverse_south_east_bend(dir),
+        _ => None,
+    }
+}
+
+pub fn map_inward_dir(dir: GridDirection, symbol: char) -> Option<GridDirection> {
+    match symbol {
+        '|' => map_inward_vertical_pipe(dir),
+        '-' => map_inward_horizontal_pipe(dir),
+        'L' => map_inward_north_east_bend(dir),
+        'J' => map_inward_north_west_bend(dir),
+        '7' => map_inward_south_west_bend(dir),
+        'F' => map_inward_south_east_bend(dir),
         _ => None,
     }
 }
 
 // symbol |
-fn vertical_pipe(dir: GridDirection) -> Option<GridDirection> {
+fn traverse_vertical_pipe(dir: GridDirection) -> Option<GridDirection> {
     match dir {
         Up => Some(Up),
         Down => Some(Down),
@@ -22,8 +34,7 @@ fn vertical_pipe(dir: GridDirection) -> Option<GridDirection> {
     }
 }
 
-// symbol -
-fn horizontal_pipe(dir: GridDirection) -> Option<GridDirection> {
+fn map_inward_vertical_pipe(dir: GridDirection) -> Option<GridDirection> {
     match dir {
         Left => Some(Left),
         Right => Some(Right),
@@ -31,8 +42,25 @@ fn horizontal_pipe(dir: GridDirection) -> Option<GridDirection> {
     }
 }
 
+// symbol -
+fn traverse_horizontal_pipe(dir: GridDirection) -> Option<GridDirection> {
+    match dir {
+        Left => Some(Left),
+        Right => Some(Right),
+        _ => None,
+    }
+}
+
+fn map_inward_horizontal_pipe(dir: GridDirection) -> Option<GridDirection> {
+    match dir {
+        Up => Some(Up),
+        Down => Some(Down),
+        _ => None,
+    }
+}
+
 // symbol L
-fn north_east_bend(dir: GridDirection) -> Option<GridDirection> {
+fn traverse_north_east_bend(dir: GridDirection) -> Option<GridDirection> {
     match dir {
         Down => Some(Right),
         Left => Some(Up),
@@ -40,8 +68,17 @@ fn north_east_bend(dir: GridDirection) -> Option<GridDirection> {
     }
 }
 
+fn map_inward_north_east_bend(dir: GridDirection) -> Option<GridDirection> {
+    match dir {
+        Up => Some(Right),
+        Down => Some(Left),
+        Left => Some(Down),
+        Right => Some(Up),
+    }
+}
+
 // symbol J
-fn north_west_bend(dir: GridDirection) -> Option<GridDirection> {
+fn traverse_north_west_bend(dir: GridDirection) -> Option<GridDirection> {
     match dir {
         Down => Some(Left),
         Right => Some(Up),
@@ -49,8 +86,17 @@ fn north_west_bend(dir: GridDirection) -> Option<GridDirection> {
     }
 }
 
+fn map_inward_north_west_bend(dir: GridDirection) -> Option<GridDirection> {
+    match dir {
+        Up => Some(Left),
+        Down => Some(Right),
+        Left => Some(Up),
+        Right => Some(Down),
+    }
+}
+
 // symbol 7
-fn south_west_bend(dir: GridDirection) -> Option<GridDirection> {
+fn traverse_south_west_bend(dir: GridDirection) -> Option<GridDirection> {
     match dir {
         Right => Some(Down),
         Up => Some(Left),
@@ -58,11 +104,29 @@ fn south_west_bend(dir: GridDirection) -> Option<GridDirection> {
     }
 }
 
+fn map_inward_south_west_bend(dir: GridDirection) -> Option<GridDirection> {
+    match dir {
+        Up => Some(Right),
+        Down => Some(Left),
+        Left => Some(Down),
+        Right => Some(Up),
+    }
+}
+
 // symbol F
-fn south_east_bend(dir: GridDirection) -> Option<GridDirection> {
+fn traverse_south_east_bend(dir: GridDirection) -> Option<GridDirection> {
     match dir {
         Left => Some(Down),
         Up => Some(Right),
         _ => None,
+    }
+}
+
+fn map_inward_south_east_bend(dir: GridDirection) -> Option<GridDirection> {
+    match dir {
+        Up => Some(Left),
+        Down => Some(Right),
+        Left => Some(Up),
+        Right => Some(Down),
     }
 }
