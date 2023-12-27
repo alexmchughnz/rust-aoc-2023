5e3d1a4bd4ee43305f79afa1b045492f9607f530
Alex McHugh <alexmchughnz@icloud.com>
2023-12-27 20:20:54 +1300

    Day 5-2: Complete.

--- src/bin/day05/main.rs
+++ src/bin/day05/main.rs
@@ -41,7 +41,7 @@ fn part_one(input: &str) -> Option<u64> {
                     return result as u64;
                 }
             }
-            return n;
+            n
         };
 
         mapper_fns.push(Box::new(mapper_fn));
@@ -62,8 +62,77 @@ fn part_one(input: &str) -> Option<u64> {
     outputs.into_iter().min()
 }
 
-fn part_two(_input: &str) -> Option<u32> {
-    None
+fn part_two(input: &str) -> Option<u64> {
+    let mut lines = input.lines();
+
+    // Extract starting seed info.
+    let seed_info: Vec<_> = lines
+        .next()
+        .unwrap()
+        .split_whitespace()
+        .filter_map(|s| s.parse::<u64>().ok())
+        .collect();
+    lines.next(); // skip blank line
+
+    // Read each map, applying the mapping rules in reverse.
+    let mut mapper_fns = Vec::<Box<dyn Fn(u64) -> u64>>::new();
+    loop {
+        let line = lines.next(); // map title, or end of input
+        println!("Skipped: {:?}", &line);
+        if line.is_none() {
+            break;
+        }
+
+        let maps: Vec<_> = lines
+            .by_ref()
+            .take_while(|line| !line.is_empty())
+            .map(|line| {
+                dbg!(line);
+                line.split_whitespace()
+                    .map(|s| s.parse::<u64>().unwrap())
+                    .collect::<Vec<_>>()
+            })
+            .collect();
+
+        let mapper_fn = move |n: u64| {
+            for map in maps.clone() {
+                let [src_start, dest_start, count] = map[..].try_into().unwrap();
+
+                if src_start <= n && n < src_start + count {
+                    let result = n as i64 + (dest_start as i64 - src_start as i64);
+                    return result as u64;
+                }
+            }
+            n
+        };
+
+        mapper_fns.push(Box::new(mapper_fn));
+    }
+
+    // Call the sequence of mapper functions in reverse on incrementing numbers until we reach a seed.
+    let mut candidate: u64 = 0;
+    let is_seed = |n: u64| {
+        let mut info = seed_info.iter();
+        while let Some(start) = info.next() {
+            let count = info.next().unwrap();
+            if *start <= n && n < start + count {
+                return true;
+            }
+        }
+        false
+    };
+
+    loop {
+        let mut value = candidate;
+        for f in mapper_fns.iter().rev() {
+            value = f(value);
+        }
+        if is_seed(value) {
+            return Some(candidate);
+        } else {
+            candidate += 1;
+        }
+    }
 }
 
 fn main() {
