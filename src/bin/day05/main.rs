77f0f69cc21f6f9e9c44af94ed06593eca52db6d
Alex McHugh <alexmchughnz@icloud.com>
2023-12-27 18:20:56 +1300

    Day 5-1: First attempt. Overflow issues on input.

--- src/bin/day05/main.rs
+++ src/bin/day05/main.rs
@@ -2,6 +2,63 @@ use aoc2023::{get_day_str, read_input};
 use std::time::Instant;
 
 fn part_one(input: &str) -> Option<u64> {
+    let mut lines = input.lines();
+
+    // Extract starting seeds.
+    let seeds = lines
+        .next()
+        .unwrap()
+        .split_whitespace()
+        .filter_map(|s| s.parse::<u64>().ok());
+    lines.next(); // skip blank line
+
+    // Read each map.
+    let mut mapper_fns = Vec::<Box<dyn Fn(u64) -> u64>>::new();
+    loop {
+        let line = lines.next(); // map title, or end of input
+        println!("Skipped: {:?}", &line);
+        if line.is_none() {
+            break;
+        }
+
+        let maps: Vec<Vec<u64>> = lines
+            .by_ref()
+            .take_while(|line| !line.is_empty())
+            .map(|line| {
+                dbg!(line);
+                line.split_whitespace()
+                    .map(|s| s.parse::<u64>().unwrap())
+                    .collect::<Vec<_>>()
+            })
+            .collect();
+
+        let mapper_fn = move |n| {
+            for map in maps.clone() {
+                let [dest_start, src_start, count] = map[..=2].try_into().unwrap();
+
+                if src_start <= n && n < src_start + count {
+                    return n + (dest_start - src_start);
+                }
+            }
+            return n;
+        };
+
+        mapper_fns.push(Box::new(mapper_fn));
+    }
+
+    // Call the sequence of mapper functions on each seed.
+    let mut outputs = Vec::new();
+    for mut seed in seeds {
+        println!("Starting seed: {}", seed);
+        for f in mapper_fns.iter() {
+            print!("{} -> ", seed);
+            seed = f(seed);
+            println!("{}", &seed);
+        }
+        outputs.push(seed);
+    }
+
+    outputs.into_iter().min()
 }
 
 fn part_two(_input: &str) -> Option<u32> {
