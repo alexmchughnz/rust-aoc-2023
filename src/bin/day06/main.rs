d09b3e99f11bd63a5795089e74fc1a10c1f75317
Alex McHugh <alexmchughnz@icloud.com>
2023-12-27 20:22:55 +1300

    Day 6-1: Complete.

--- src/bin/day06/main.rs
+++ src/bin/day06/main.rs
@@ -1,8 +1,42 @@
 use aoc2023::{get_day_str, read_input};
 use std::time::Instant;
 
-fn part_one(_input: &str) -> Option<u32> {
-    None
+fn part_one(input: &str) -> Option<u32> {
+    let mut lines = input.lines();
+    let times: Vec<u32> = lines
+        .next()
+        .unwrap()
+        .split_whitespace()
+        .skip(1)
+        .map(|s| s.parse::<u32>().unwrap())
+        .collect(); // [milliseconds]
+    let records: Vec<u32> = lines
+        .next()
+        .unwrap()
+        .split_whitespace()
+        .skip(1)
+        .map(|s| s.parse::<u32>().unwrap())
+        .collect(); // [millimeters]
+
+    let mut num_winning_combos = Vec::new();
+    for race in 0..times.len() {
+        let mut num_wins = 0;
+        let total_time = times[race];
+
+        for hold_time in 1..total_time {
+            let travel_time = total_time - hold_time;
+            let speed = hold_time;
+
+            let distance = speed * travel_time;
+            if distance > records[race] {
+                num_wins += 1;
+            }
+        }
+
+        num_winning_combos.push(num_wins);
+    }
+
+    Some(num_winning_combos.into_iter().product())
 }
 
 fn part_two(_input: &str) -> Option<u32> {
