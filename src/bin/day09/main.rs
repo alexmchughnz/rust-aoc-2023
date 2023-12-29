806f5e2db4981ca943bf9b99c2494fb747d9821b
Alex McHugh <alexmchughnz@icloud.com>
2023-12-30 12:16:25 +1300

    Day 9-1: Complete.

--- src/bin/day09/main.rs
+++ src/bin/day09/main.rs
@@ -1,8 +1,39 @@
 use aoc2023::{get_day_str, read_input};
 use std::time::Instant;
 
-fn part_one(_input: &str) -> Option<u32> {
-    None
+fn part_one(input: &str) -> Option<u32> {
+    let lines = input.lines();
+
+    let mut predictions = Vec::<_>::new();
+    for line in lines {
+        let nums = line
+            .split_whitespace()
+            .map(|s| s.parse::<i32>().unwrap())
+            .collect::<Vec<_>>();
+
+        // Generate sequences of diffs.
+        let mut all_diffs = Vec::<Vec<_>>::new();
+        all_diffs.push(nums);
+        while !all_diffs.last().unwrap().iter().all(|&d| d == 0) {
+            let next_diffs = all_diffs.last().unwrap().windows(2).map(|w| w[1] - w[0]);
+            all_diffs.push(next_diffs.collect());
+        }
+        assert!(all_diffs.last().unwrap().iter().all(|&d| d == 0));
+
+        // Interpolate diffs to predict next value.
+        let mut delta = 0;
+        for diffs in all_diffs.iter_mut().rev() {
+            let end = diffs.last().unwrap();
+            let new = end + delta;
+            diffs.push(new);
+
+            delta = new;
+        }
+
+        predictions.push(delta);
+    }
+
+    Some(predictions.into_iter().sum::<i32>() as u32)
 }
 
 fn part_two(_input: &str) -> Option<u32> {
