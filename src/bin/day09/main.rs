9b567522608e70c387f07f887c267c327817f4a8
Alex McHugh <alexmchughnz@icloud.com>
2023-12-30 14:06:06 +1300

    Day 9-2: Complete.

--- src/bin/day09/main.rs
+++ src/bin/day09/main.rs
@@ -36,8 +36,39 @@ fn part_one(input: &str) -> Option<u32> {
     Some(predictions.into_iter().sum::<i32>() as u32)
 }
 
-fn part_two(_input: &str) -> Option<u32> {
-    None
+fn part_two(input: &str) -> Option<u32> {
+    let lines = input.lines();
+
+    let mut predictions = Vec::<_>::new();
+    for line in lines {
+        let nums = line
+            .split_whitespace()
+            .map(|s| s.parse::<i32>().unwrap())
+            .collect::<Vec<_>>();
+
+        // Generate sequences of diffs.
+        let mut all_diffs = Vec::<Vec<_>>::new();
+        all_diffs.push(nums.into());
+        while !all_diffs.last().unwrap().iter().all(|&d| d == 0) {
+            let next_diffs = all_diffs.last().unwrap().windows(2).map(|w| w[1] - w[0]);
+            all_diffs.push(next_diffs.collect());
+        }
+        assert!(all_diffs.last().unwrap().iter().all(|&d| d == 0));
+
+        // Interpolate diffs to predict *previous* value.
+        let mut delta = 0;
+        for diffs in all_diffs.iter_mut().rev() {
+            let start = diffs.first().unwrap();
+            let new = start - delta;
+            diffs.insert(0, new);
+
+            delta = new;
+        }
+
+        predictions.push(delta);
+    }
+
+    Some(predictions.into_iter().sum::<i32>() as u32)
 }
 
 fn main() {
