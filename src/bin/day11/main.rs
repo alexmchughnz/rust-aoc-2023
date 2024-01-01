bd665aa7d4c2958b76e6ee8ebcca43021fdd4b42
Alex McHugh <alexmchughnz@icloud.com>
2024-01-01 14:57:38 +1300

    Day 11: Scaffold.

---
+++ src/bin/day11/main.rs
@@ -0,0 +1,31 @@
+use aoc2023::{get_day_str, read_input};
+use std::time::Instant;
+
+use aoc2023::helpers::grid::Grid;
+
+fn part_one(input: &str) -> Option<u32> {
+    None
+}
+
+fn part_two(_input: &str) -> Option<u32> {
+    None
+}
+
+fn main() {
+    let input = read_input(file!(), "input.txt");
+    let day = get_day_str(file!());
+
+    let time = Instant::now();
+    println!(
+        "{day}-1 solution: {:?} | {:.2?}",
+        part_one(&input),
+        time.elapsed()
+    );
+
+    let time = Instant::now();
+    println!(
+        "{day}-2 solution: {:?} | {:.2?}",
+        part_two(&input),
+        time.elapsed()
+    );
+}
