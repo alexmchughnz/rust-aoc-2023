459422a10f9adf3891a530ca4fecb9d2dd4106b6
Alex McHugh <alexmchughnz@icloud.com>
2024-01-01 19:07:47 +1300

    Day 11-2: Complete.

--- src/bin/day11/main.rs
+++ src/bin/day11/main.rs
@@ -64,8 +64,67 @@ fn part_one(input: &str) -> Option<u32> {
     Some(distances.into_iter().sum::<usize>() as u32)
 }
 
-fn part_two(_input: &str) -> Option<u32> {
-    None
+const EXPANSION_SIZE: usize = 999_999;
+fn part_two(input: &str) -> Option<u64> {
+    let image: Grid<char> = input.parse().unwrap();
+    dbg!(&image);
+
+    // Find all galaxies.
+    let mut galaxies = Vec::new();
+    for (index, c) in image.iter() {
+        if *c == '#' {
+            galaxies.push(index);
+        }
+    }
+
+    // Locate indices of double-width rows/columns.
+    let expanded_rows = (0..image.height())
+        .into_iter()
+        .filter(|&i| {
+            galaxies
+                .iter()
+                .all(|galaxy_index| galaxy_index.indices.0 != i)
+        })
+        .collect::<Vec<_>>();
+
+    let expanded_columns = (0..image.width())
+        .into_iter()
+        .filter(|&j| {
+            galaxies
+                .iter()
+                .all(|galaxy_index| galaxy_index.indices.1 != j)
+        })
+        .collect::<Vec<_>>();
+
+    // Determine distance between each pair of galaxies, counting doubled rows/columns twice.
+    let mut distances = Vec::new();
+    for n in 1..galaxies.len() {
+        let (start, rest) = galaxies.split_at(n);
+        let a = start.last().unwrap().indices;
+        for b in rest.into_iter().map(|index| index.indices) {
+            let mut row_indices = [a.0, b.0];
+            row_indices.sort();
+
+            let mut column_indices = [a.1, b.1];
+            column_indices.sort();
+
+            let num_expanded_rows = expanded_rows
+                .iter()
+                .filter(|&&i| row_indices[0] < i && i < row_indices[1])
+                .count();
+            let num_expanded_columns = expanded_columns
+                .iter()
+                .filter(|&&j| column_indices[0] < j && j < column_indices[1])
+                .count();
+
+            let i_distance = row_indices[1] - row_indices[0] + num_expanded_rows * EXPANSION_SIZE;
+            let j_distance =
+                column_indices[1] - column_indices[0] + num_expanded_columns * EXPANSION_SIZE;
+            distances.push(i_distance + j_distance);
+        }
+    }
+
+    Some(distances.into_iter().sum::<usize>() as u64)
 }
 
 fn main() {
